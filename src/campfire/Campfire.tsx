713e26a715467c8fb7e8ea7b9344c772e9aaa57e
Dean-Baylem <dbaylem5822@gmail.com>
2026-06-07 12:46:49 +0900

    Added model and base shader material for floor

--- src/campfire/Campfire.tsx
+++ src/campfire/Campfire.tsx
@@ -1,4 +1,7 @@
 import { Center, useGLTF } from "@react-three/drei";
+import Fire from "./fire/Fire";
+import floorVertexShader from "./shaders/floor/vertex.glsl";
+import floorFragmentShader from "./shaders/floor/fragment.glsl";
 
 export default function Campfire() {
     const { scene } = useGLTF("/models/campfire/campfire.glb");
@@ -8,7 +11,10 @@ export default function Campfire() {
             <primitive object={scene} />
             <mesh rotation={[-Math.PI / 2, 0, 0]}>
                 <circleGeometry args={[2.5, 32]} />
-                <shaderMaterial />
+                <shaderMaterial
+                    vertexShader={floorVertexShader}
+                    fragmentShader={floorFragmentShader}
+                />
             </mesh>
             {/* <Fire /> */}
         </Center>
